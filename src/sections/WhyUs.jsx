d89ddc9ba26c6c6898b68d3c5c03a37437b5f566
Daniel Leite <daniel.f.m.leite@gmail.com>
2024-11-05 08:17:44 +0000

    feedback improvements on the why us page

--- src/sections/WhyUs.jsx
+++ src/sections/WhyUs.jsx
@@ -8,12 +8,12 @@ const WhyUs = () => {
         <section className="pt-12 bg-white">
             <Element name="Why Healix">
                 <div className="container">
-                    <h2 className="mb-6 font-semibold text-6xl text-p4 max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+                    <h2 className="font-semibold text-6xl text-p4 max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
                         Why Healix?
                     </h2>
 
-                    <div className="relative flex md:flex-wrap flex-nowrap  rounded-7xl md:overflow-hidden max-md:flex-col feature-after max-md:border-none max-md:rounded-none max-md:gap-5">
-                        <div className="max-w-4xl mx-auto p-4 basis-1/2">
+                    <div className="flex gap-12 md:flex-nowrap sm:flex-wrap md:overflow-hidden max-md:flex-col max-md:border-none max-md:rounded-none max-md:gap-5">
+                        <div className="max-w-4xl mx-auto p-4 basis-1/2 mt-auto">
                             <div className="grid grid-cols-11 gap-3 mb-3 text-center text-white">
                                 <div className="bg-p4 rounded-lg p-4 font-bold col-span-3 opacity-0"></div>
                                 <div className="bg-p4 tracking-wider rounded-lg p-4 font-bold col-span-4">Healix</div>
@@ -32,26 +32,28 @@ const WhyUs = () => {
                                         <div className="p-4 bg-orange-100 text-sm text-gray-700 col-span-4 rounded">
                                             {healixInfo}
                                         </div>
-                                        <div className="p-4 bg-white text-sm text-gray-700 col-span-4 rounded">
+                                        <div className="p-4 bg-s1 text-sm text-gray-700 col-span-4 rounded">
                                             {conventionMedicineInfo}
                                         </div>
                                     </div>
                                 ))}
                             </div>
                         </div>
-                        <div className="basis-1/2 mt-3">
+                        <div className="basis-1/2 mt-3 mb-3">
                             {reasons.map(({ id, title, text }) => (
                                 <div
                                     key={id}
-                                    className="relative z-2 mb-6 md:px-6 px-5 basis-1/1 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
+                                    className="relative z-2 mb-6  basis-1/1 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
                                 >
-                                    <h2 className="mb-1 h6 text-p4 max-md:mb-3 max-md:h5">{title}</h2>
+                                    <h2 className="mb-1 text-2xl text-p4 max-md:mb-3 max-md:h5">{title}</h2>
                                     <p className="body-3 max-md:mb-8 max-md:body-3">
                                         <CheckIcon className="size-6 text-s6 inline mr-2" />
                                         {text}
                                     </p>
                                 </div>
                             ))}
+
+                            <img src="../images/why-us/doctor-conventional.png" className="rounded-xl" />
                         </div>
                     </div>
                 </div>
